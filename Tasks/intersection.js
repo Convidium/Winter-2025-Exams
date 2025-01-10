1af320c2b664d18ff5a819fee3870a9c6a8f3202
Convidium <denyssianchuk0@gmail.com>
2025-01-10 18:04:01 +0200

    intersection.js: Step 1

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,26 +1,21 @@
 // Refactor following solution
 // Find an intersection of two dictionaries
 
-intersection = function intersection(object_1, object_2) {
+// Step 1:
+// change function name to `checkIntersection` (makes much more sense what it's intentions are)
+// change function to labmbda function
+// remove useless curly braces
+
+checkIntersection = (object_1, object_2) => {
   first_keys = Object.keys(object_1);
-  {
-  }
   for (attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
-      {
-      }
     } else {
       delete object_1[attribute_name];
-      {
-      }
     }
-    {
-    }
-  }
-  {
   }
   return object_1;
 };
 
-module.exports = intersection;
+module.exports = checkIntersection;
