fe639bb3d268c837390e5f3eb139d7bf145bd0d2
Convidium <denyssianchuk0@gmail.com>
2025-01-10 16:25:52 +0200

    between.js: Step 2

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,25 +1,17 @@
 // Refactor following solution
 // Extract substring between prefix and suffix
 
-// Step 1: Fix variable namings
-// str to string
-// p to prefix
-// s to suffix
+// Step 2: Remove unnecessary else statement (also fix another variable naming)
 
 getvaluebetween = (string, prefix, suffix) => {
   prefixIndex = string.indexOf(prefix);
-  if (prefixIndex === -1) return '';
-  else {
-    k = prefixIndex + prefix.length;
-    string = string.substring(k);
-    if (suffix) {
-      prefixIndex = string.indexOf(suffix);
-      if (prefixIndex === -1) {
-        return '';
-      } else {
-        string = string.substring(0, prefixIndex);
-      }
-    }
+  if (prefixIndex === -1) return "";
+  suffixIndex = prefixIndex + prefix.length;
+  string = string.substring(suffixIndex);
+  if (suffix) {
+    prefixIndex = string.indexOf(suffix);
+    if (prefixIndex === -1) return "";
+    string = string.substring(0, prefixIndex);
   }
   return string;
 };
