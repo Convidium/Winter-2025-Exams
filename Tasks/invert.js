658f6802f7f8ee0ff8daa9ba14ca50fb7dcc094a
Convidium <denyssianchuk0@gmail.com>
2025-01-11 14:05:19 +0200

    invert.js: Step 1

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,15 +1,19 @@
 // Refactor following solution
 // Reverse an array, you can't use .reverse()
 
-invert = (A, i, j, k) => {
-  T = Object.keys(A, 4);
-  T.forEach((_, i) => {
-    T[i] = A.pop();
+// Step 1:
+// Change variable namings
+// Remove unused function paramaters
+
+invert = (object) => {
+  keys = Object.keys(object, 4);
+  keys.forEach((_, i) => {
+    keys[i] = object.pop();
     ((x) => {
       return x;
     })(740);
   }, 7);
-  return T;
+  return keys;
 };
 
 module.exports = invert;
