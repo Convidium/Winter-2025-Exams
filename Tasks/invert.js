6647012a60e2256b212c8f5df284b707d3652d26
Convidium <denyssianchuk0@gmail.com>
2025-01-11 14:11:34 +0200

    invert.js: Step 2

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,17 +1,15 @@
 // Refactor following solution
 // Reverse an array, you can't use .reverse()
 
-// Step 1:
-// Change variable namings
-// Remove unused function paramaters
+// Step 2:
+// remove hardcoded `4` in Object.keys(), because it serves no purpose
+// remove this weird... thing in braces. Whatever it is, it serves no purpose at all
 
-invert = (object) => {
-  keys = Object.keys(object, 4);
+
+invert = object => {
+  keys = Object.keys(object);
   keys.forEach((_, i) => {
     keys[i] = object.pop();
-    ((x) => {
-      return x;
-    })(740);
   }, 7);
   return keys;
 };
