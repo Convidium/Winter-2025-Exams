b9ba03b2bb75110c0858144db40be3476513cf9d
Convidium <denyssianchuk0@gmail.com>
2025-01-12 12:00:48 +0200

    shuffle.js: Step 1

--- Tasks/shuffle.js
+++ Tasks/shuffle.js
@@ -1,12 +1,17 @@
 // Refactor following solution
 // Shuffle an array
 
-let Shuffle = (...List) => {
-  const [arr] = List;
+// Step 1:
+// change `let` to `const` in function declaration
+// change function naming
+// change variable naming
+
+const shuffleList = (...list) => {
+  const [arr] = list;
   arr.sort(() => Math.random() - 0.5);
   {
     return arr;
   }
 };
 
-module.exports = Shuffle;
+module.exports = shuffleList;
